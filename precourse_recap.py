8150ae5ad7fb7172b29940881419c91564bac25c
moath al-abdulrazaq <moath@moaths-MacBook-Air.local>
2022-05-17 13:27:11 +0100

    new file has been added

---
+++ precourse_recap.py
@@ -0,0 +1,17 @@
+x = 12
+r = "moath"
+y = 12.4 
+u = False 
+
+b = x + y 
+print(b)
+moath = [x ,r ,y ,u]
+print(moath)
+
+print(r.upper()) 
+
+if x < y:
+    print("yes,12,4 is bigger than 12") 
+
+else : 
+    print("no way") 
